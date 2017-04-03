8fb089aaca2c3dfa65e0b5042f9318cd8efb036a
iryu54 <corentinlabroche@gmail.com>
2017-04-03 18:35:22 +0000

    rename

--- partition.sh
+++ partition.sh
@@ -7,4 +7,4 @@ mkfs.ext4 ${partition}
 mount ${partition} /mnt
 pacstrap /mnt base base-devel git
 genfstab -U -p /mnt >> /mnt/etc/fstab
-arch-chroot /mnt git clone https://github.com/clabroche/archlinux_install.git && archlinux_install/first&reboot.sh
+arch-chroot /mnt git clone https://github.com/clabroche/archlinux_install.git && archlinux_install/first_script.sh
