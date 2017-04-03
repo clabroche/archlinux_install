d3d79ff3ccd2d1a14215d58b5d4554d3239f717d
iryu54 <corentinlabroche@gmail.com>
2017-04-03 21:21:59 +0000

    noconfirm to pacman

--- first_script.sh
+++ first_script.sh
@@ -1,10 +1,10 @@
-pacman -S sudo grub os-prober
+pacman -S sudo grub os-prober --noconfirm
 useradd -g users -G wheel,storage,power -m coco
 grub-mkconfig -o /boot/grub/grub.cfg
 cd archlinux_install
 rsync -a etc/ /etc
-pacman -Syu
-pacman -S bash-completion networkmanager lxdm wpa_supplicant netctl dialog net-tools
+pacman -Syu --noconfirm
+pacman -S bash-completion networkmanager lxdm wpa_supplicant netctl dialog net-tools --noconfirm
 sudo systemctl enable dhcpcd
 locale-gen
 echo LANG="fr_FR.UTF-8" > /etc/locale.conf
--- yaourt.sh
+++ yaourt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 git clone https://aur.archlinux.org/package-query.git
 cd package-query
-makepkg -si
+makepkg -si --noconfirm
 git clone https://aur.archlinux.org/yaourt.git
 cd yaourt
-makepkg -si
+makepkg -si --noconfirm
 cd ../../
 sudo rm -r package-query
